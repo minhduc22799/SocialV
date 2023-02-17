ee82191a01d66b5def2b549bd7125118f3d1e29c
minhduc22799 <minhduc22799@gmail.com>
2023-02-17 10:08:30 +0700

    commit admin

---
+++ FE/socialV/src/app/admin-management/admin-management.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+import {Title} from "@angular/platform-browser";
+
+@Component({
+  selector: 'app-admin-management',
+  templateUrl: './admin-management.component.html',
+  styleUrls: ['./admin-management.component.css']
+})
+export class AdminManagementComponent implements OnInit{
+  constructor() {
+  }
+  ngOnInit(): void {
+
+  }
+
+}
