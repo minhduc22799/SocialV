16e07c07dcf7d9b6c684919d617818a735585345
viethoang9999 <viethoang9999>
2023-02-20 08:11:43 +0700

    block + BE search user

--- BE/SocialV/src/main/java/com/example/socialv/service/admin/AdminService.java
+++ BE/SocialV/src/main/java/com/example/socialv/service/admin/AdminService.java
@@ -1,14 +1,19 @@
 package com.example.socialv.service.admin;
 
 import com.example.socialv.model.Users;
+import com.example.socialv.repository.IRoleRepository;
 import com.example.socialv.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AdminService {
     @Autowired
     private IUserRepository iUserRepository;
+    @Autowired
+    private IRoleRepository iRoleRepository;
 
     public boolean blockAndUnLockUsers(Users users) {
         if (!users.isStatus()) {
@@ -21,5 +26,17 @@ public class AdminService {
             return true;
         }
     }
-
+    public boolean loginAdmin(Users users) {
+        List<Users> usersList = iUserRepository.findAll();
+        for (Users us : usersList) {
+            if (us.getUsername().equals(users.getUsername()) && us.getPassword().equals(users.getPassword()) && us.getRole()==iRoleRepository.findById(2l).get()) {
+                us.setCheckOn(true);
+                return true;
+            }
+        }
+        return false;
+    }
+       public List<Users> findAllUser(){
+            return iUserRepository.showAllUser();
+        }
 }
