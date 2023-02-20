54c8e9db6fe451f58ccfb10e20b5486b40d22846
minhduc22799 <minhduc22799@gmail.com>
2023-02-20 08:15:06 +0700

    commit done comment,like,like comment all trang

---
+++ FE/socialV/src/app/post-detail/post-detail.component.ts
@@ -0,0 +1,368 @@
+import { Component } from '@angular/core';
+import {Users} from "../Model/Users";
+import {PostDisplay} from "../Model/Post-display";
+import {ImagePost} from "../Model/image-post";
+import {PostComment} from "../Model/post-comment";
+import {PostStatus} from "../Model/post-status";
+import {Post} from "../Model/Post";
+import {FormControl, FormGroup} from "@angular/forms";
+import {AngularFireStorage, AngularFireStorageReference} from "@angular/fire/compat/storage";
+import {NavigationEnd, Router} from "@angular/router";
+import {PostService} from "../PostService/post.service";
+import {UserService} from "../service/user.service";
+import * as moment from "moment/moment";
+import Swal from "sweetalert2";
+
+@Component({
+  selector: 'app-post-detail',
+  templateUrl: './post-detail.component.html',
+  styleUrls: ['./post-detail.component.css']
+})
+export class PostDetailComponent {
+
+  data = localStorage.getItem("user")
+  // @ts-ignore
+  user: Users = JSON.parse(this.data)
+  postsDisplay: PostDisplay[] = [];
+  listFriend: Users[] = [];
+  listImgPost: ImagePost[][] = [];
+  listFriendPost: Users[][] = [];
+  listImg: any[] = [];
+  listComment: PostComment[] = [];
+  listAllComment: PostComment[][] = [];
+  listCommentLike: number[][] = [];
+  listCheckLikeComment: boolean[][] = [];
+  countLike: any[] = [];
+  countComment: any[] = [];
+  listPostStatus: PostStatus[] = [];
+  listImgCreate: ImagePost[] = [];
+  imageFiles: any[] = [];
+  imgSrc: string[] = [];
+  timeMoment: any[] = []
+  timeMomentComment: any[][] = []
+  listRequest: Users[] = [];
+  pathName!: string
+  flag!: false;
+  postCm?:Post
+  commentP?:PostComment
+  showMore: boolean = false;
+
+  commentForm:FormGroup = new FormGroup({
+    content: new FormControl()
+  })
+
+  commentFormEdit:FormGroup = new FormGroup({
+    id:new FormControl(),
+    content: new FormControl(),
+    cmtAt: new FormControl(),
+    post: new FormGroup({
+      id:new FormControl()
+    })
+  })
+
+  showMoreItems() {
+    this.showMore = true;
+  }
+  showLessItems() {
+    this.showMore = false;
+  }
+
+  postForm: FormGroup = new FormGroup({
+    content: new FormControl(),
+    postStatus: new FormGroup({
+      id: new FormControl("1")
+    })
+  })
+  // upload file c2
+  arrFileInFireBase: AngularFireStorageReference | undefined
+  checkUploadMultiple = false;
+
+  ngOnInit(): void {
+    // @ts-ignore
+    this.findAll()
+    this.findAllFriend()
+    this.getAllPostStatus()
+    // this.onMoveTop()
+    this.findListRequest()
+  }
+
+  onMoveTop() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    })
+  }
+
+  constructor(private postService: PostService,
+              private userService: UserService,
+              private storage: AngularFireStorage,
+              private router: Router) {
+
+  }
+
+
+  findAllFriend() {
+    // @ts-ignore
+    this.userService.findAllFriend(this.user.id).subscribe((data) => {
+      this.listFriend = data
+    })
+  }
+
+  findAll() {
+    this.postService.findAllPostNewFeed(this.user).subscribe((post) => {
+      this.postsDisplay = post
+      for (let j = 0; j < this.postsDisplay.length; j++) {
+        this.timeMoment.push(moment(this.postsDisplay[j].createAt).fromNow())
+      }
+      this.findAllImgPost(post)
+      this.findFriendLike(post)
+      this.findCountLike(post)
+      this.findCountComment(post)
+      this.getAllListComment(post)
+
+    })
+  }
+
+  findFriendLike(posts: Post[]) {
+    this.postService.findLikePost(posts).subscribe(like => {
+      this.listFriendPost = like
+    })
+  }
+
+  findCountLike(posts: Post[]) {
+    this.postService.findCountLikePost(posts).subscribe(countLike => {
+      this.countLike = countLike
+    })
+  }
+
+  deleteRequest(friendRequestId: any) {
+    this.userService.deleteRequest(this.user.id, friendRequestId).subscribe(() => {
+      this.findListRequest()
+    })
+  }
+
+  confirmRequest(friendRequestId: any) {
+    this.userService.confirmRequest(this.user.id, friendRequestId).subscribe(() => {
+      this.findListRequest()
+    })
+  }
+
+  findCountComment(posts: Post[]) {
+    this.postService.findCountCommentPost(posts).subscribe(countComment => {
+      this.countComment = countComment
+    })
+  }
+
+  addComment(post:Post){
+
+    const postComment = this.commentForm.value
+    postComment.users = this.user
+    postComment.post = post
+
+    this.postService.addComment(postComment).subscribe(() =>{
+      this.findAll()
+      this.commentForm.reset()
+    })
+  }
+
+
+  getCommentById(id:number){
+    this.postService.getCommentById(id).subscribe((data)=>{
+      this.commentP = data
+      this.commentFormEdit.patchValue(data)
+      console.log(data)
+    })
+  }
+  editComment(){
+    const postComment = this.commentFormEdit.value
+    postComment.users = this.user
+    postComment.cmtAt = this.commentP?.cmtAt
+    // @ts-ignore
+    this.postService.editComment(this.commentP.id,postComment).subscribe(() =>{
+      this.findAll()
+      document.getElementById("edit-comment")?.click()
+    })
+  }
+
+  deleteComment(id:number){
+    this.postService.deleteComment(id).subscribe(()=>{
+      this.findAll()
+    })
+  }
+
+
+  getAllListComment(posts: Post[]) {
+    this.postService.getAllListComment(posts).subscribe(data => {
+      this.listAllComment = data
+      // console.log(moment(data[2][1].cmtAt).fromNow())
+      for (let e = 0; e < data.length; e++){
+        if (data[e].length > 0) {
+          this.timeMomentComment[e] = []
+          for (let f = 0; f < data[e].length; f++) {
+            // @ts-ignore
+            this.timeMomentComment[e][f] = moment(data[e][f].cmtAt).fromNow()
+          }
+        }else {
+          this.timeMomentComment[e] = []
+        }
+      }
+      this.getListCommentLike()
+      this.getListCheckLikeComment()
+    })
+  }
+
+  likeComment(id:number){
+    // @ts-ignore
+    this.postService.likeComment(this.user.id, id).subscribe(()=>{
+      this.findAll()
+    })
+  }
+
+
+  findAllImgPost(posts: Post[]) {
+    this.postService.findAllImgPost(posts).subscribe(img => {
+      this.listImgPost = img
+      for (let i = 0; i < this.listImgPost.length; i++) {
+        // @ts-ignore
+        this.listImg[i] = [];
+        for (let j = 0; j < this.listImgPost[i].length; j++) {
+          // @ts-ignore
+          let imageObject1 = {
+            image: this.listImgPost[i][j].img,
+            thumbImage: this.listImgPost[i][j].img,
+          };
+          this.listImg[i].push(imageObject1);
+        }
+      }
+    })
+  }
+
+  getCommentByIdPost(id: number) {
+    this.postService.getListComment(id).subscribe(data => {
+      this.listComment = data
+    })
+  }
+
+  getAllPostStatus() {
+    this.postService.getAllPostStatus().subscribe(data => {
+      this.listPostStatus = data;
+    })
+  }
+
+  createPost() {
+    const post = this.postForm.value
+    post.users = this.user
+    this.postService.createPost(post).subscribe(data => {
+      // @ts-ignore
+      if (this.imageFiles.length === 0) {
+        this.postForm.reset();
+        this.findAll();
+        document.getElementById("btn-close")?.click()
+        Swal.fire(
+          'Good job!',
+          'You clicked the button!',
+          'success'
+        )
+      } else {
+        this.createPostImg(data)
+      }
+    })
+  }
+
+  submitAvatar(event: any) {
+    this.imageFiles = event.target.files;
+    for (let i = 0; i < this.imageFiles.length; i++) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.imgSrc.push(e.target.result);
+      };
+      reader.readAsDataURL(this.imageFiles[i]);
+    }
+  }
+
+  i = 0;
+
+  createPostImg(post: Post) {
+    if (this.imageFiles !== undefined) {
+      this.checkUploadMultiple = true
+      this.arrFileInFireBase = this.storage.ref(this.imageFiles[this.i].name);
+      this.arrFileInFireBase.put(this.imageFiles[this.i]).then(data => {
+        return data.ref.getDownloadURL();
+      }).then(url => {
+        this.checkUploadMultiple = false
+        let imagePost: ImagePost = {
+          post: post,
+          img: url
+        }
+        this.listImgCreate.push(imagePost)
+      }).then(() => {
+        this.i++
+        if (this.i < this.imageFiles.length) {
+          this.createPostImg(post)
+        } else {
+          this.i = 0
+          this.postService.createPostImg(this.listImgCreate).subscribe(() => {
+            this.findAll()
+          })
+          document.getElementById("btn-close")?.click()
+          this.postForm.reset();
+          this.imgSrc = []
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            text: 'Post successfully posted',
+            showConfirmButton: false,
+            timer: 1500
+          })
+        }
+      })
+    }
+  }
+
+
+
+  findListRequest() {
+    // @ts-ignore
+    this.userService.findListRequestFriend(this.user.id).subscribe((data) => {
+      this.listRequest = data
+
+    })
+  }
+
+  likePost(idPost?: number) {
+    this.postService.likePost(this.user.id, idPost).subscribe(() => {
+      this.findAll()
+    })
+  }
+
+  deleteImgCreate(id: any | undefined) {
+    this.imgSrc.splice(id, 1);
+    let a: any[] = []
+    for (let i = 0; i < this.imageFiles.length; i++) {
+      if (i != id) {
+        a.push(this.imageFiles[i])
+      }
+    }
+    this.imageFiles = a
+  }
+
+  logOut() {
+    localStorage.removeItem("user");
+    this.router.navigate(['']);
+
+  }
+
+  getListCommentLike(){
+    this.postService.getCountComment(this.listAllComment).subscribe(data=>{
+      this.listCommentLike = data
+    })
+  }
+
+  getListCheckLikeComment() {
+    this.postService.getCheckLikeComment(this.listAllComment, this.user.id).subscribe(data => {
+      this.listCheckLikeComment = data
+    })
+  }
+}
+
