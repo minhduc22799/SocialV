de95be0b60037538e516737199a7e11b14600dd6
minhduc22799 <minhduc22799@gmail.com>
2023-02-22 08:24:03 +0700

    commit sprint fix

--- FE/socialV/src/app/Friend-Profile/friend-profile.component.ts
+++ FE/socialV/src/app/Friend-Profile/friend-profile.component.ts
@@ -272,6 +272,13 @@ export class FriendProfileComponent implements OnInit {
       this.sendNotification()
     })
   }
+
+  deleteRequestById(friendRequestId: any) {
+    this.userService.deleteRequest(this.user.id, friendRequestId).subscribe(() => {
+      this.findListRequest()
+      this.sendNotification()
+    })
+  }
   confirmRequest(friendRequestId: any){
     this.userService.confirmRequest(this.user.id, friendRequestId).subscribe(()=>{
       this.findFriendOfFriend()
